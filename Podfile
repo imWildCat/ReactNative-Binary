15c94022035c909a70382d5a68b168fc06e6e72e
WildCat <wildcat.name@gmail.com>
2022-06-18 10:11:27 +0800

    Update initial template

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+require_relative './frontend/node_modules/react-native/scripts/react_native_pods'
+require_relative './frontend/node_modules/@react-native-community/cli-platform-ios/native_modules'
+
+target 'DummyApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  use_react_native!(path: './frontend/node_modules/react-native')
+end
---
+++ scripts/build_xcframework.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# export PATH="/usr/local/bin:$PATH"
+# export PATH="/opt/homebrew/bin/:$PATH"
+
+exclude_frameworks=("PINCache" "PINOperation" "PINRemoteImage" "Pods_DummyApp" "DummyApp")
+
+function archive() {
+  xcodebuild clean archive \
+    -workspace $PROJECT.xcworkspace \
+    -scheme $PROJECT \
+    -archivePath $SRCROOT/$PROJECT-iphonesimulator.xcarchive \
+    -sdk iphonesimulator \
+    SKIP_INSTALL=NO
+
+  xcodebuild clean archive \
+    -workspace $PROJECT.xcworkspace \
+    -scheme $PROJECT \
+    -archivePath $SRCROOT/$PROJECT-iphoneos.xcarchive \
+    -sdk iphoneos \
+    SKIP_INSTALL=NO
+}
+
+function create_xcframework() {
+  rm -rf $SRCROOT/Frameworks
+  mkdir $SRCROOT/Frameworks
+
+  # Find frameworks
+  for framework in $(find $SRCROOT/$PROJECT-iphonesimulator.xcarchive/Products/Library/Frameworks -type d -name "*.framework"); do
+    basename=$(basename $framework)
+    framework_name=$(basename $framework .framework)
+
+    if [[ " ${exclude_frameworks[*]} " =~ " ${framework_name} " ]]; then
+      continue
+    fi
+
+    xcodebuild -create-xcframework \
+      -framework $SRCROOT/$PROJECT-iphonesimulator.xcarchive/Products/Library/Frameworks/$basename \
+      -framework $SRCROOT/$PROJECT-iphoneos.xcarchive/Products/Library/Frameworks/$basename \
+      -output $SRCROOT/Frameworks/$framework_name.xcframework
+  done
+
+  # Find bundle resources
+  for resources in $(find $BUILT_PRODUCTS_DIR/../.. -type d -name "*.bundle"); do
+    cp -R $resources $SRCROOT/Frameworks/
+  done
+
+  tar -cvzf $PROJECT.tar.gz Frameworks
+}
+
+function clean() {
+  # Clean Up
+  rm -rf $SRCROOT/$PROJECT-iphoneos.xcarchive
+  rm -rf $SRCROOT/$PROJECT-iphonesimulator.xcarchive
+  rm -rf $SRCROOT/Frameworks
+}
+
+function distribute() {
+  echo "dummy release"
+  # gh release create "$1" --generate-notes -R "imWildCat/DummyApp"
+  # gh release upload "$1" $PROJECT.tar.gz -R "imWildCat/DummyApp" && rm -rf $PROJECT.tar.gz
+
+  # pod repo push imWildCat DummyApp.podspec --verbose --allow-warnings --skip-tests
+}
+
+cd $SRCROOT
+# version=$(cat DummyApp.podspec | grep version | sed -n 's/version.=."\(.*\)".*/\1/p' | xargs)
+version="1.0"
+
+archive
+create_xcframework
+clean
+distribute $version
