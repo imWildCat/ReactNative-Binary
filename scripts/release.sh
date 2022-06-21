95e8a7ed7d09f59fa019b8c58e5019b6af6dc64b
WildCat <wildcat.name@gmail.com>
2022-06-21 19:16:29 +0800

    adjust release script to allow addition version

--- scripts/release.sh
+++ scripts/release.sh
@@ -28,4 +28,11 @@ function distribute() {
 
 react_native_version=$(cat frontend/package.json | grep react-native | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]' | sed 's/[^0-9.]//g')
 
-distribute $react_native_version
+release_branch_version=$(echo "$BRANCH_NAME" | sed 's/releases\///g')
+
+if [[ $release_branch_version =~ ^$react_native_version ]]; then
+  distribute "$release_branch_version"
+else
+  echo "release branch version must begins with react_native_version" >&2
+  exit 1
+fi
